9cab76e4c6c89cc899e8c46294b468f1eead2abc
Erasmo J.F Da Silva <30526854+Erasmojf@users.noreply.github.com>
2022-05-17 13:19:16 -0300

    Criando ByteCoin App
    Criando ByteCoin App

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Erasmo J.F Da Silva on 16/05/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
